66c253cdf04631c4e8cb393962d4002ed4a4cd97
dgjinsu <rlawlstn3206@naver.com>
2024-11-16 18:22:08 +0900

    feat: swagger에 Bearer Token 삽입 기능 추가

--- DemoWithCleanArchitecture/WebAPI/Program.cs
+++ DemoWithCleanArchitecture/WebAPI/Program.cs
@@ -1,4 +1,6 @@
 using infrastructure.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,7 +8,43 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(swagger =>
+{
+    swagger.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "ASP.NET 8 Web API",
+        Description = "Authentication with JWT"
+    });
+
+    // To Enable authorization using Swagger (JWT)
+    swagger.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "Enter JWT token without 'Bearer ' prifix"
+    });
+
+    swagger.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
+
+
 builder.Services.InfrastructureServices(builder.Configuration);  // 의존성 주입 구성한 클래스
 
 var app = builder.Build();
